fc28cf23b91deb5fbfcc909f9cf955dcdd184568
Elvis Estevan <elvis.estevan65@gmail.com>
2018-04-04 17:31:49 -0300

    Lendo do teclado com o Scan

--- hello.go
+++ hello.go
@@ -1,16 +1,21 @@
 package main
 
 import "fmt"
-import "reflect"
 
 func main() {
 	nome := "Elvis"
-	idade := 28
 	versao := 1.1
-	fmt.Println("Olá, sr.", nome, "sua idade é", idade, "anos")
-	fmt.Println("Este programa está na versão", versao)
 
-	fmt.Println("Tipo nome:", reflect.TypeOf(nome))
-	fmt.Println("Tipo idade:", reflect.TypeOf(idade))
-	fmt.Println("Tipo versao:", reflect.TypeOf(versao))
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+	fmt.Println("\n1. Iniciar monitoramento")
+	fmt.Println("2. Exibir os logs")
+	fmt.Println("0. Sair")
+	fmt.Print("\nInforme: ")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	fmt.Println(comando)
+
 }
