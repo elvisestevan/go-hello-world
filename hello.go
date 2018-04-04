0e4cc00692b3b4dcbf41f466519b225431b7b7f4
Elvis Estevan <elvis.estevan65@gmail.com>
2018-04-04 16:54:42 -0300

    Tipos de variaveis

--- hello.go
+++ hello.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	var nome string = "Elvis"
+	var idade int = 28
+	var versao float32 = 1.1
+	fmt.Println("Olá, sr.", nome, "sua idade é", idade, "anos")
+	fmt.Println("Este programa está na versão", versao)
 }
